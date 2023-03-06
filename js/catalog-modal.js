9040a502a1ab960d497939bab2993b58fc7a63fc
Kryazh113 <skat_113@mail.ru>
2023-03-06 08:47:36 +0300

    завершение проекта

--- js/catalog-modal.js
+++ js/catalog-modal.js
@@ -7,9 +7,16 @@ for(let i=0; i<cardName.length; i++){
     evt.preventDefault;
     catalogModal.classList.add('catalog-modal-show');
   })
-}
+};
 
 closeModalButton.addEventListener('click', function(evt){
   evt.preventDefault;
   catalogModal.classList.remove('catalog-modal-show');
-})
+});
+
+window.addEventListener('keydown',function(evt){
+  if(evt.keyCode === 27){
+    evt.preventDefault;
+    catalogModal.classList.remove('catalog-modal-show');
+  }
+});
